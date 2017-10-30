2197ce60ab7c8928a3c4dcd17bab75719b9c3fbb
avenash97 <avenash.r15@iiits.in>
2017-10-31 01:18:38 +0530

    new changes

--- itsclient/urls.py
+++ itsclient/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
+import django.views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
