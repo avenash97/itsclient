1d6164df1cef574be0810fbea13c0baf98875d77
avenash97 <avenash.r15@iiits.in>
2017-10-31 16:20:39 +0530

    make changes

--- itsclient/settings.py
+++ itsclient/settings.py
@@ -10,13 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+
 import os
 import os.path
 # Build paths inside the project like this:os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
